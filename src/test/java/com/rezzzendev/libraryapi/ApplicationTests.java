4b244ee3b5e7a8669a2989877bf200659bad1a50
Matheus Rezende <rezzzendev@gmail.com>
2025-05-12 20:00:27 -0300

    Initial commit

---
+++ src/test/java/com/rezzzendev/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezzzendev.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
