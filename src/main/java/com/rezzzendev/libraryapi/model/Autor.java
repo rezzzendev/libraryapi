93c15d953da22ad34be116568b96691d88b3d57c
Matheus Rezende <matheus.rezende@unimedanapolis.com.br>
2025-05-13 10:52:50 -0300

    Primeira Entidade

---
+++ src/main/java/com/rezzzendev/libraryapi/model/Autor.java
@@ -0,0 +1,34 @@
+package com.rezzzendev.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    @Deprecated
+    public Autor() {
+
+    }
+}
